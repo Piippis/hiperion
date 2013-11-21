533fd252e7d5f296f1d482e9c5996dd5823bfa70
Haydenish <antipaste@gmail.com>
2013-11-21 14:57:02 +0200

    Boo

--- server.go
+++ server.go
@@ -3,26 +3,45 @@ package main
 import (
 	//"fmt"
 	"github.com/fzzy/radix/redis"
+	"github.com/gorilla/sessions"
 	"github.com/gorilla/mux"
 	"html/template"
 	"log"
+	"net/http"
 )
 
 var db *redis.Client
+var store = sessions.NewCookieStore(
+	[]byte(SESSION_AUTHENTICATION),
+	[]byte(SESSION_ENCRYPTION),
+)
 
-func homeHandler(c http.ResponseWriter, req *http.Request) {
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	session, _ = store.Get(req, "hiperion")
+	if req.FormValue("name") != "" {
+		session.Values["name"] = req.FormValue("name")
+		session.Save(req, w)
+	}
 	homeTemplate := template.Must(template.ParseFiles("home.html"))
-	homeTemplate.Execute(c, nil)
+	homeTemplate.Execute(w, struct {
+		Name string
+	}{
+		Name: session.Values["name"]
+	})
 }
 
-func staticHandler(c http.ResponseWriter, req *http.Request) {
-	http.ServeFile(c, req, req.URL.Path[1:])
+func staticHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		http.Error(w, "Invalid method", 405)
+	}
+
+	http.ServeFile(w, req, req.URL.Path[1:])
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", homeHandler)
-	router.HandleFunc("/static/", staticHandler).Methods("GET")
+	router.HandleFunc("/static/", staticHandler)
 
 	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
 	if err != nil {
@@ -34,7 +53,7 @@ func main() {
 
 	log.Println("Server started!")
 
-	if err := http.ListenAndServe(":1234", nil); err != nil {
+	if err := http.ListenAndServe(":1234", router); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
